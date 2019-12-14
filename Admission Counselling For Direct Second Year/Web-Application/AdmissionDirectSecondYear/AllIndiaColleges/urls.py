e98ba12e07aa1220a31130ea7ff02da46eef1330
Atharva Satyendra Agrawal <atharva7199990@gmail.com>
2019-12-14 21:51:50 +0530

    Delete urls.py

--- Admission Counselling For Direct Second Year/Web-Application/AdmissionDirectSecondYear/AllIndiaColleges/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('All-India-Colleges-2019/',views.allIndiaCollege,name="allIndiaCollege"),
-    path('All-India-Colleges-2018/',views.allIndiaCollege2018,name="allIndiaCollege")
-]
