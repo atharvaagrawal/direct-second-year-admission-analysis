26a482183670c0e326c1a452c2d23c2f92a1648c
Atharva Satyendra Agrawal <atharva7199990@gmail.com>
2019-12-14 21:51:26 +0530

    Delete views.py

--- Admission Counselling For Direct Second Year/Web-Application/AdmissionDirectSecondYear/AllIndiaColleges/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-
-def allIndiaCollege(request):
-    return HttpResponse("All-India-Colleges-2019");
-
-def allIndiaCollege2018(request):
-    return HttpResponse("All-India-Colleges-2018");
