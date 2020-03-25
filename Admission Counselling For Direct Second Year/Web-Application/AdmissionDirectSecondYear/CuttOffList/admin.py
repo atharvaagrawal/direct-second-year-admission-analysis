c29d3114c23388eec20a6917b172d51ebcb6417d
Atharva Satyendra Agrawal <atharva7199990@gmail.com>
2020-03-25 14:25:24 +0530

    Delete admin.py

--- Admission Counselling For Direct Second Year/Web-Application/AdmissionDirectSecondYear/CuttOffList/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from . models import CuttOffListModel
-# Register your models here.
-
-#admin.site.register(TopCollegesModel)
-
-class CuttOffListAdminData(admin.ModelAdmin):
-    list_display = ('institute_code','choice_code')
-admin.site.register(CuttOffListModel , CuttOffListAdminData )
