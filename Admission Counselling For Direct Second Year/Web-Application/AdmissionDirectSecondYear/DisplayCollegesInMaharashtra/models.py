6e28fe76702cde79fca68e92ab3145bb754ae9a2
Atharva Satyendra Agrawal <atharva7199990@gmail.com>
2020-03-25 14:56:36 +0530

    Add files via upload

--- Admission Counselling For Direct Second Year/Web-Application/AdmissionDirectSecondYear/DisplayCollegesInMaharashtra/models.py
+++ Admission Counselling For Direct Second Year/Web-Application/AdmissionDirectSecondYear/DisplayCollegesInMaharashtra/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class DisplayCollegesInMaharashtraModel(models.Model):
     choice_code = models.IntegerField()
     course_name = models.CharField(max_length=300)
-    shift = models.IntegerField()
+    shift = models.CharField(max_length=300)
     seat_intake = models.IntegerField()
     college_name = models.CharField(max_length=300)
 
