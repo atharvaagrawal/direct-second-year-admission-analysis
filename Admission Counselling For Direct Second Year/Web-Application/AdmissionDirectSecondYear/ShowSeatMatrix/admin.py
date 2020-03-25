cb3c3579910b2eaa05647b19d83510236428e1c8
Atharva Satyendra Agrawal <atharva7199990@gmail.com>
2020-03-25 14:47:51 +0530

    Delete admin.py

--- Admission Counselling For Direct Second Year/Web-Application/AdmissionDirectSecondYear/ShowSeatMatrix/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from . models import ShowSeatMatrixModel
-# Register your models here.
-
-class ShowSeatMatrixAdminData(admin.ModelAdmin):
-    list_display = ('institute_code','choice_code','vacant_seats','lateral_seats')
-admin.site.register(ShowSeatMatrixModel , ShowSeatMatrixAdminData )
- 
