6a38bd4a89bfe2b12ce32be84d55d9b9e9476dab
Atharva Satyendra Agrawal <atharva7199990@gmail.com>
2020-10-02 21:41:42 +0530

    Update views.py

--- Admission Counselling For Direct Second Year/Web-Application/AdmissionDirectSecondYear/WhereICanGetAdmission/views.py
+++ Admission Counselling For Direct Second Year/Web-Application/AdmissionDirectSecondYear/WhereICanGetAdmission/views.py
@@ -21,17 +21,24 @@ def college_i_can_get_admission(request):
     category_get = ""
     college_get = ""
     branch_get = ""
+    percentage_get = ""
+
     result = zip() 
     if request.method == "POST":
         if request.POST.get("Vise"):    
             category_get = request.POST['category']
             branch_get = request.POST['branch']
             college_get = request.POST['college']
+            percentage_get = str(request.POST['percentage'])
 
-        if college_get != "Select College Name":
+        if percentage_get !='0' and college_get != "Select College Name" and category_get != "Select Category":
+            result = WhereAdmissionModel.objects.raw("select * from collegerange2019 where min_percentage <= %s AND college_name = %s AND category = %s order by college_name", (percentage_get,college_get,category_get))
+        elif college_get != "Select College Name":
             result =  WhereAdmissionModel.objects.raw("select * from collegerange2019 where college_name = %s  order by category  ",(college_get,))
         elif category_get != "Select Category":
             result =  WhereAdmissionModel.objects.raw("select * from collegerange2019 where category = %s order by college_name ",(category_get,))
+        elif percentage_get !=0:
+            result = WhereAdmissionModel.objects.raw("select * from collegerange2019 where min_percentage <= %s order by college_name ", (percentage_get,))
            
         
         if request.POST.get("All"):
